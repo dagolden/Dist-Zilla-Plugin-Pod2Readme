cc874d39a3199c6b099eda344446d3960399e1d6
David Golden <xdg@xdg.me>
2017-04-02 13:32:48 -0400

    After release: bump $VERSION and timestamp Changes

--- lib/Dist/Zilla/Plugin/Pod2Readme.pm
+++ lib/Dist/Zilla/Plugin/Pod2Readme.pm
@@ -5,7 +5,7 @@ use warnings;
 package Dist::Zilla::Plugin::Pod2Readme;
 # ABSTRACT: Generate README from Pod, as simply as possible
 
-our $VERSION = '0.004';
+our $VERSION = '0.005';
 
 use Dist::Zilla 5 ();
 
